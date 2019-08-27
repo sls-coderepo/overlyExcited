07a96497b5a060809144c974c1514caecfe5595e
unknown <[shrestha_sl@hotmail.com]>
2019-08-27 11:02:31 -0500

    practice stacking words

--- js/overly-excited.js
+++ js/overly-excited.js
@@ -1,1 +1,13 @@
 console.log("Hello, I am excited to learn javaScript!")
+
+let sentence = ["The","walrus","danced","through","the","trees","in","the","light","of","the","moon"];
+// concatinating the string
+function addExcitement(theWordArray) {
+    let buildMeUp = " "
+    for (let i = 0; i < theWordArray.length; i++){
+        buildMeUp = buildMeUp.concat(theWordArray[i], " ");
+        console.log(buildMeUp);       
+    }
+}
+
+addExcitement(sentence);
