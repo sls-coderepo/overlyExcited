6434c7d5224cb05f38c40548506a34d02906d68c
unknown <[shrestha_sl@hotmail.com]>
2019-08-27 13:54:12 -0500

    new argument added

--- js/overly-excited.js
+++ js/overly-excited.js
@@ -2,16 +2,17 @@ console.log("Hello, I am excited to learn javaScript!")
 
 let sentence = ["The","walrus","danced","through","the","trees","in","the","light","of","the","moon"];
 
-// concatinating the character
-function addExcitement(theWordArray) {
+// adding new argument
+function addExcitement(theWordArray, seperator) {
     let buildMeUp = " "
     for (let i = 0; i < theWordArray.length; i++){
         buildMeUp = buildMeUp.concat(theWordArray[i], " ");
         if ((i + 1) % 3 === 0) {
-            buildMeUp = buildMeUp.concat("! ");
+            buildMeUp = buildMeUp.concat(seperator);
         }
         console.log(buildMeUp);       
     }
 }
 
-addExcitement(sentence);
+addExcitement(sentence, "?")
+
