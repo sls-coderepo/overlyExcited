4f608773d8b49ff5e025f3a019e961c94fccb9c0
unknown <[shrestha_sl@hotmail.com]>
2019-08-27 17:03:05 -0500

    added all functions in master

--- js/overly-excited.js
+++ js/overly-excited.js
@@ -2,17 +2,54 @@ console.log("Hello, I am excited to learn javaScript!")
 
 let sentence = ["The","walrus","danced","through","the","trees","in","the","light","of","the","moon"];
 
-// repeat string multiple times
-function addExcitement(theWordArray, seperator, numOfTimes) {
+// concatinating the strings
+function addExcitement(theWordArray) {
     let buildMeUp = " "
     for (let i = 0; i < theWordArray.length; i++){
         buildMeUp = buildMeUp.concat(theWordArray[i], " ");
+        console.log(buildMeUp);       
+    }
+}
+addExcitement(sentence);
+
+// concatinating the character
+function addExcitement_char(theWordArray) {
+    let buildMeUp = " "
+    for (let i = 0; i < theWordArray.length; i++){
+        buildMeUp = buildMeUp.concat(theWordArray[i]);
         if ((i + 1) % 3 === 0) {
-            buildMeUp = buildMeUp.concat(seperator.repeat(numOfTimes));
+            buildMeUp = buildMeUp.concat("! ");
+        }
+        console.log(buildMeUp);       
+    }
+}
+addExcitement_char(sentence);
+
+// adding new argument
+function addExcitement_separator(theWordArray, seperator) {
+    let buildMeUp = " "
+    for (let i = 0; i < theWordArray.length; i++){
+        buildMeUp = buildMeUp.concat(theWordArray[i]);
+        if ((i + 1) % 3 === 0) {
+            buildMeUp = buildMeUp.concat(seperator," ");
+        }
+        console.log(buildMeUp);       
+    }
+}
+addExcitement_separator(sentence, "?")
+
+
+// repeat string multiple times
+function addExcitement_times(theWordArray, seperator, numOfTimes) {
+    let buildMeUp = " "
+    for (let i = 0; i < theWordArray.length; i++){
+        buildMeUp = buildMeUp.concat(theWordArray[i]);
+        if ((i + 1) % 3 === 0) {
+            buildMeUp = buildMeUp.concat(seperator.repeat(numOfTimes), " ");
         }
         console.log(buildMeUp);       
     }
 }
 
-addExcitement(sentence, "*", 4)
+addExcitement_times(sentence, "*", 4)
 
