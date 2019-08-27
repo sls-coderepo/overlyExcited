68d7142940652b7bf69e4a81d02e6014381821d7
unknown <[shrestha_sl@hotmail.com]>
2019-08-27 12:04:17 -0500

    exclamation mark added

--- js/overly-excited.js
+++ js/overly-excited.js
@@ -1,1 +1,17 @@
 console.log("Hello, I am excited to learn javaScript!")
+
+let sentence = ["The","walrus","danced","through","the","trees","in","the","light","of","the","moon"];
+
+// concatinating the character
+function addExcitement(theWordArray) {
+    let buildMeUp = " "
+    for (let i = 0; i < theWordArray.length; i++){
+        buildMeUp = buildMeUp.concat(theWordArray[i], " ");
+        if ((i + 1) % 3 === 0) {
+            buildMeUp = buildMeUp.concat("! ");
+        }
+        console.log(buildMeUp);       
+    }
+}
+
+addExcitement(sentence);
